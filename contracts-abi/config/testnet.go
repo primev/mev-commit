c754f72b0c478a84ee900524d86bf889b83cd740
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2024-04-12 11:24:34 -0400

    chore: update contract addresses

--- contracts-abi/config/testnet.go
+++ contracts-abi/config/testnet.go
@@ -9,7 +9,7 @@ type Contracts struct {
 
 var TestnetContracts = Contracts{
 	BidderRegistry:         "0x02CcEcB19c6D7EFe583C8b97022cB4b4C0B65608",
-	ProviderRegistry:       "0x070cE6161AD79a3BC7aEa222FdfC6AD171Ca83F3",
-	PreconfCommitmentStore: "0x4DfF34f74aE5C48a5050eb54e7cEDAb9DEF03715",
-	Oracle:                 "0x6bD961a0c15057983F6b457319187782fc98FcAc",
+	ProviderRegistry:       "0x48D4521ac3537256042568Bc5BCD77be10c094cc",
+	PreconfCommitmentStore: "0x5dBbe982640B3029A8A3422efe9dDD3faAc8A591",
+	Oracle:                 "0xD4821CBFAeC31BE139cC2D5B0874460f684B237B",
 }
