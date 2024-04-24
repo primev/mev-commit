01135d6498a9916bef7727fdbab62133912af48e
Alok <aloknerurkar@no-reply.com>
2024-04-24 18:32:34 +0530

    chore: moving publisher to events pkg

--- x/contracts/events/publisher/ws_test.go
+++ x/contracts/events/publisher/ws_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/google/go-cmp/cmp"
-	"github.com/primevprotocol/mev-commit/x/contracts/publisher"
+	"github.com/primevprotocol/mev-commit/x/contracts/events/publisher"
 	"github.com/primevprotocol/mev-commit/x/util"
 )
 
