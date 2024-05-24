f5569a57065d1e66e518070396c5d8428aa1e623
Shawn <44221603+shaspitz@users.noreply.github.com>
2024-05-24 15:02:50 -0700

    fix: set default value to contracts env var

--- contracts/entrypoint.sh
+++ contracts/entrypoint.sh
@@ -11,11 +11,10 @@ SCRIPT_PATH_PREFIX=${SCRIPT_PATH_PREFIX:-scripts/}
 
 KEYSTORE_PASSWORD=${KEYSTORE_PASSWORD:-"pwd"}
 
-# Check if CONTRACT_REPO_ROOT_PATH is set, if so, prepare the --root option
-ROOT_OPTION=""
-if [ -n "$CONTRACT_REPO_ROOT_PATH" ]; then
-    ROOT_OPTION="--root $CONTRACT_REPO_ROOT_PATH"
-fi
+CONTRACT_REPO_ROOT_PATH=${CONTRACT_REPO_ROOT_PATH:-$(pwd)}
+
+# TODO: Whole issue could be around the fact that we're not deploying from contracts repo directly!!
+# Try cding into contracts repo root path
 
 if [ "$DEPLOY_TYPE" = "core" ]; then
     echo "Deploying core contracts"
@@ -24,14 +23,14 @@ if [ "$DEPLOY_TYPE" = "core" ]; then
         --priority-gas-price 2000000000 \
         --with-gas-price 5000000000 \
         --rpc-url "$RPC_URL" \
-        --keystores ./deployer_keystore/* \
+        --keystores "$CONTRACT_REPO_ROOT_PATH"/deployer_keystore/* \
         --password "$KEYSTORE_PASSWORD" \
         --sender "a51f13769d1466e0b5483cb719e89add8d615052" \
         --broadcast \
         --chain-id "$CHAIN_ID" \
         -vvvv \
         --use 0.8.23 \
-        $ROOT_OPTION \
+        --root "$CONTRACT_REPO_ROOT_PATH" \
         --via-ir
 
 elif [ "$DEPLOY_TYPE" = "whitelist" ]; then
@@ -43,14 +42,15 @@ elif [ "$DEPLOY_TYPE" = "whitelist" ]; then
     HYP_ERC20_ADDR="$HYP_ERC20_ADDR" $FORGE_BIN_PATH script \
         "${SCRIPT_PATH_PREFIX}"DeployScripts.s.sol:DeployWhitelist \
         --rpc-url "$RPC_URL" \
-        --keystores ./deployer_keystore/* \
+        --keystores "$CONTRACT_REPO_ROOT_PATH"/deployer_keystore/* \
         --password "$KEYSTORE_PASSWORD" \
         --sender "a51f13769d1466e0b5483cb719e89add8d615052" \
         --broadcast \
         --chain-id "$CHAIN_ID" \
         -vvvv \
         --use 0.8.23 \
-        $ROOT_OPTION
+        --root "$CONTRACT_REPO_ROOT_PATH" \
+        --via-ir
 
 elif [ "$DEPLOY_TYPE" = "settlement-gateway" ]; then
     if [ -z "$RELAYER_ADDR" ]; then
@@ -61,14 +61,15 @@ elif [ "$DEPLOY_TYPE" = "settlement-gateway" ]; then
     RELAYER_ADDR="$RELAYER_ADDR" $FORGE_BIN_PATH script \
         "${SCRIPT_PATH_PREFIX}"DeployStandardBridge.s.sol:DeploySettlementGateway \
         --rpc-url "$RPC_URL" \
-        --keystores ./deployer_keystore/* \
+        --keystores "$CONTRACT_REPO_ROOT_PATH"/deployer_keystore/* \
         --password "$KEYSTORE_PASSWORD" \
         --sender "a51f13769d1466e0b5483cb719e89add8d615052" \
         --broadcast \
         --chain-id "$CHAIN_ID" \
         -vvvv \
         --use 0.8.23 \
-        $ROOT_OPTION
+        --root "$CONTRACT_REPO_ROOT_PATH" \
+        --via-ir
 
 elif [ "$DEPLOY_TYPE" = "l1-gateway" ]; then
     if [ -z "$RELAYER_ADDR" ]; then
@@ -79,14 +80,15 @@ elif [ "$DEPLOY_TYPE" = "l1-gateway" ]; then
     RELAYER_ADDR="$RELAYER_ADDR" $FORGE_BIN_PATH script \
         "${SCRIPT_PATH_PREFIX}"DeployStandardBridge.s.sol:DeployL1Gateway \
         --rpc-url "$RPC_URL" \
-        --keystores ./deployer_keystore/* \
+        --keystores "$CONTRACT_REPO_ROOT_PATH"/deployer_keystore/* \
         --password "$KEYSTORE_PASSWORD" \
         --sender "a51f13769d1466e0b5483cb719e89add8d615052" \
         --broadcast \
         --chain-id "$CHAIN_ID" \
         -vvvv \
         --use 0.8.23 \
-        $ROOT_OPTION
+        --root "$CONTRACT_REPO_ROOT_PATH" \
+        --via-ir
 
 elif [ "$DEPLOY_TYPE" = "validator-registry" ]; then
     echo "Deploying validator registry contract"
@@ -96,13 +98,13 @@ elif [ "$DEPLOY_TYPE" = "validator-registry" ]; then
         --with-gas-price 5000000000 \
         "${SCRIPT_PATH_PREFIX}"DeployScripts.s.sol:DeployValidatorRegistry \
         --rpc-url "$RPC_URL" \
-        --keystores ./deployer_keystore/* \
+        --keystores "$CONTRACT_REPO_ROOT_PATH"/deployer_keystore/* \
         --password "$KEYSTORE_PASSWORD" \
         --sender "a51f13769d1466e0b5483cb719e89add8d615052" \
         --broadcast \
         --chain-id "$CHAIN_ID" \
         -vvvv \
         --use 0.8.23 \
-        $ROOT_OPTION \
+        --root "$CONTRACT_REPO_ROOT_PATH" \
         --via-ir
 fi 
