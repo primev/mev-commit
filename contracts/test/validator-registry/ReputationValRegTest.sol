27efa0632d5565daddf2f5b0cae2680a690b4ea6
Shawn <44221603+shaspitz@users.noreply.github.com>
2024-06-03 15:26:44 -0700

    test: add testDeleteWhitelistedEOA

--- contracts/test/validator-registry/ReputationValRegTest.sol
+++ contracts/test/validator-registry/ReputationValRegTest.sol
@@ -4,12 +4,15 @@ pragma solidity ^0.8.20;
 import "forge-std/Test.sol";
 import "../../contracts/validator-registry/ReputationValReg.sol";
 import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
 
 contract ReputationValRegTest is Test {
     ReputationValReg public reputationValReg;
     address public owner;
     address public user1;
     address public user2;
+    address public user3;
 
     uint256 public constant MAX_CONS_ADDRS_PER_EOA = 5;
     uint256 public constant MIN_FREEZE_BLOCKS = 100;
@@ -29,6 +32,7 @@ contract ReputationValRegTest is Test {
         owner = address(0x789);
         user1 = address(0x123);
         user2 = address(0x456);
+        user3 = address(0x777);
         
         assertEq(exampleConsAddr1.length, 48);
         assertEq(exampleConsAddr2.length, 48);
@@ -47,8 +51,33 @@ contract ReputationValRegTest is Test {
         reputationValReg.initialize(owner, MAX_CONS_ADDRS_PER_EOA, MIN_FREEZE_BLOCKS, UNFREEZE_FEE);
         vm.stopPrank();
     }
+    
+    function testNonWhitelistedEOA() public view {
+        assertFalse(reputationValReg.isEOAWhitelisted(user1));
+        (ReputationValReg.State state, uint256 numConsAddrsStored, 
+            uint256 freezeHeight, string memory moniker) = reputationValReg.getWhitelistedEOAInfo(user1);
+        assertEq(uint256(state), uint256(ReputationValReg.State.NotWhitelisted));
+        assertEq(numConsAddrsStored, 0);
+        assertEq(freezeHeight, 0);
+        assertEq(moniker, "");
+    }
+
+    function testNonExistantConsAddr() public view{
+        // TODO
+    }
 
     function testAddWhitelistedEOA() public {
+
+        vm.startPrank(user1);
+        vm.expectRevert(abi.encodeWithSelector(OwnableUpgradeable.OwnableUnauthorizedAccount.selector, user1));
+        reputationValReg.addWhitelistedEOA(address(0), "bob");
+        vm.stopPrank();
+
+        vm.startPrank(owner);
+        vm.expectRevert("Invalid address");
+        reputationValReg.addWhitelistedEOA(address(0), "bob");
+        vm.stopPrank();
+
         uint256 initialOwnerBalance = owner.balance;
         vm.startPrank(owner);
         vm.expectEmit(true, true, true, true);
@@ -65,19 +94,10 @@ contract ReputationValRegTest is Test {
         assertEq(freezeHeight, 0);
         assertEq(moniker, "bob");
 
-        initialOwnerBalance = owner.balance;
         vm.startPrank(owner);
         vm.expectRevert("EOA must not already be whitelisted");
         reputationValReg.addWhitelistedEOA(user1, "bob");
         vm.stopPrank();
-        assertEq(owner.balance, initialOwnerBalance);
-
-        initialOwnerBalance = owner.balance;
-        vm.startPrank(owner);
-        vm.expectRevert("Invalid address");
-        reputationValReg.addWhitelistedEOA(address(0), "alice");
-        vm.stopPrank();
-        assertEq(owner.balance, initialOwnerBalance);
 
         initialOwnerBalance = owner.balance;
         vm.startPrank(owner);
@@ -94,4 +114,48 @@ contract ReputationValRegTest is Test {
         assertEq(freezeHeight2, 0);
         assertEq(moniker2, "alice");
     }
+
+    function testDeleteWhitelistedEOA() public {
+        testAddWhitelistedEOA();
+
+        vm.startPrank(user3);
+        vm.expectRevert("Only owner or EOA itself can delete whitelisted EOA");
+        reputationValReg.deleteWhitelistedEOA(user1);
+        vm.expectRevert("EOA must be whitelisted");
+        reputationValReg.deleteWhitelistedEOA(user3);
+        vm.stopPrank();
+
+        assertTrue(reputationValReg.isEOAWhitelisted(user1));
+        assertTrue(reputationValReg.isEOAWhitelisted(user2));
+
+        vm.startPrank(owner);
+        vm.expectEmit(true, true, true, true);
+        emit WhitelistedEOADeleted(user1, "bob");
+        reputationValReg.deleteWhitelistedEOA(user1);
+        vm.stopPrank();
+
+        assertFalse(reputationValReg.isEOAWhitelisted(user1));
+        assertTrue(reputationValReg.isEOAWhitelisted(user2));
+        (ReputationValReg.State state1, uint256 numConsAddrsStored1, 
+            uint256 freezeHeight1, string memory moniker1) = reputationValReg.getWhitelistedEOAInfo(user1);
+        assertEq(uint256(state1), uint256(ReputationValReg.State.NotWhitelisted));
+        assertEq(numConsAddrsStored1, 0);
+        assertEq(freezeHeight1, 0);
+        assertEq(moniker1, "");
+        (ReputationValReg.State state2, uint256 numConsAddrsStored2, 
+            uint256 freezeHeight2, string memory moniker2) = reputationValReg.getWhitelistedEOAInfo(user2);
+        assertEq(uint256(state2), uint256(ReputationValReg.State.Active));
+        assertEq(numConsAddrsStored2, 0);
+        assertEq(freezeHeight2, 0);
+        assertEq(moniker2, "alice");
+
+        vm.startPrank(user2);
+        vm.expectEmit(true, true, true, true);
+        emit WhitelistedEOADeleted(user2, "alice");
+        reputationValReg.deleteWhitelistedEOA(user2);
+        vm.stopPrank();
+
+        assertFalse(reputationValReg.isEOAWhitelisted(user2));
+    }
+
 }
