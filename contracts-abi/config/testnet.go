9bf81ab9308afdd55cac5f283e7f1746b6cfbd7e
mrekucci <mrekucci@users.noreply.github.com>
2024-06-17 18:28:48 +0200

    refactor: update testnet contracts addresses (#156)

--- contracts-abi/config/testnet.go
+++ contracts-abi/config/testnet.go
@@ -12,9 +12,9 @@ var TestnetContracts = Contracts{
 	// If these addresses change for a testnet deployment,
 	// please also update snippets/testnet-addresses.mdx
 	// in https://github.com/primev/mev-commit-docs
-	BidderRegistry:         "0x8021D1a949E124E39F9FFc812F39054e1521dAed",
-	ProviderRegistry:       "0x9C108C6F4A6350B42B4Cf0384249A0BB2F57e12D",
-	PreconfCommitmentStore: "0xb4FFB66e8a913F6Bcc7FbF4d27BC2b1545A0811b",
-	Oracle:                 "0xfC9a86CcC74F63AFE1c1d4A1aC25B2e248b234F6",
-	BlockTracker:           "0x2816c159E9F0807a5121e39dA4969de3b6D8d25e",
+	BidderRegistry:         "0x2eEbF31f5c932D51556E70235FB98bB2237d065c",
+	ProviderRegistry:       "0x4FC9b98e1A0Ff10de4c2cf294656854F1d5B207D",
+	PreconfCommitmentStore: "0xCAC68D97a56b19204Dd3dbDC103CB24D47A825A3",
+	Oracle:                 "0x6856Eb630C79D491886E104D328834643B3F69E3",
+	BlockTracker:           "0x2eEbF31f5c932D51556E70235FB98bB2237d065c",
 }
