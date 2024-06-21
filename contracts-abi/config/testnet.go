4b995217886f5463a02502d41121ea446f03c239
Shawn <44221603+shaspitz@users.noreply.github.com>
2024-06-21 10:47:27 -0700

    fix: contract addrs (optional)

--- contracts-abi/config/testnet.go
+++ contracts-abi/config/testnet.go
@@ -12,9 +12,9 @@ var TestnetContracts = Contracts{
 	// If these addresses change for a testnet deployment,
 	// please also update snippets/testnet-addresses.mdx
 	// in https://github.com/primev/mev-commit-docs
-	BidderRegistry:         "0xC383f487A93eD43a0dC85f9f63069ce37767f875",
-	ProviderRegistry:       "0x1Ca3e5398228b4E6dA1f8b7f176b2a797b9B33bf",
-	PreconfCommitmentStore: "0x2Aa7b59E07A92908570b79f68addf2915e5586e5",
-	Oracle:                 "0xAaA1e7B1CE47910FA3CE79FC18c57cC12322991E",
-	BlockTracker:           "0xABa711EdEC215972d8Da0C65DDc3E092754e691c",
+	BidderRegistry:         "0xd44adA804c53a7eE42145f752daD8fBa4a521D50",
+	ProviderRegistry:       "0x1714b4E6b60FE34f0ec39e428150944D80af7E63",
+	PreconfCommitmentStore: "0x56f6A527B07Dc9980dE4609F06887cB498903A0D",
+	Oracle:                 "0xc5958e569556b54B25DfE4ad6B3CD4690a5db039",
+	BlockTracker:           "0xF4C89c9851A613a447470CCe6866923C6e14F041",
 }
