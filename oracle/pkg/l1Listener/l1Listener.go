a6094d7032b5b49d28146ed157ebb0c2f379a8f8
Kartik Chopra <k5chopra@uwaterloo.ca>
2024-07-05 14:03:54 -0400

    feat: adds execution enforcement to preconfirmations (#161)
    * feat: adds more txn metadata to block cache
    
    * feat: slash on non-successful transactions
    
    * chore: remove redundant log
    
    * feat: request receipts concurrently
    
    * feat: cleanup code
    
    * feat: adds txn receipts to tests
    
    * chore: updates test stub to include txn receipts
    
    * feat: adds a revert check in tests
    
    * feat: use errgroup and syncmap
    
    * feat: introduces batching
    
    * feat: rework bucketing to minimize requests
    
    * chore: resolves nit PR requests
    
    * chore: removes closure var
    
    * chore: use range over integer
    
    * feat: adds metrics
    
    * chore: adds metric to collector
    
    * feat: get total duration for block receipts
    
    * feat: adds retry to BatchReceipts call
    
    * feat: adds retries to all calls
    
    * feat: check for 429 in infinite retry
    
    * feat: do infinite retry on batch call and log
    
    * feat: checks for 429 errors
    
    * feat: attempt the request multiple times
    
    * feat: attempt retries 50 times before failing
    
    * feat: reduce the size of batch
    
    * feat: adds personal L1 RPC URL
    
    * chore: adds temp debug logs
    
    * chore: avoid context from cancelation
    
    * chore: panic when updater routine fails
    
    * feat: adds logger for evmhelper
    
    * feat: add more logs
    
    * feat: fix linter issues

--- oracle/pkg/l1Listener/l1Listener.go
+++ oracle/pkg/l1Listener/l1Listener.go
@@ -27,6 +27,7 @@ type WinnerRegister interface {
 type EthClient interface {
 	BlockNumber(ctx context.Context) (uint64, error)
 	HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error)
+	BlockByNumber(ctx context.Context, number *big.Int) (*types.Block, error)
 }
 
 type L1Listener struct {
--- oracle/pkg/node/node.go
+++ oracle/pkg/node/node.go
@@ -106,7 +106,7 @@ func NewNode(opts *Options) (*Node, error) {
 	monitor := txmonitor.New(
 		owner,
 		settlementClient,
-		txmonitor.NewEVMHelper(settlementClient.Client()),
+		txmonitor.NewEVMHelperWithLogger(settlementClient.Client(), nd.logger),
 		st,
 		nd.logger.With("component", "tx_monitor"),
 		1024,
@@ -163,6 +163,8 @@ func NewNode(opts *Options) (*Node, error) {
 	if opts.LaggerdMode > 0 {
 		listenerL1Client = &laggerdL1Client{EthClient: listenerL1Client, amount: opts.LaggerdMode}
 	}
+
+	listenerL1Client = &infiniteRetryL1Client{EthClient: listenerL1Client, logger: nd.logger}
 
 	blockTracker, err := blocktracker.NewBlocktrackerTransactor(
 		opts.BlockTrackerContractAddr,
@@ -232,10 +234,11 @@ func NewNode(opts *Options) (*Node, error) {
 
 	updtr, err := updater.NewUpdater(
 		nd.logger.With("component", "updater"),
-		l1Client,
+		listenerL1Client,
 		st,
 		evtMgr,
 		oracleTransactorSession,
+		txmonitor.NewEVMHelperWithLogger(l1Client.Client(), nd.logger),
 	)
 	if err != nil {
 		nd.logger.Error("failed to instantiate updater", "error", err)
@@ -403,6 +406,61 @@ func (w *winnerOverrideL1Client) HeaderByNumber(ctx context.Context, number *big
 	return hdr, nil
 }
 
+type infiniteRetryL1Client struct {
+	l1Listener.EthClient
+	logger *slog.Logger
+}
+
+func (i *infiniteRetryL1Client) BlockNumber(ctx context.Context) (uint64, error) {
+	var blkNum uint64
+	var err error
+	for retries := 50; retries > 0; retries-- {
+		blkNum, err = i.EthClient.BlockNumber(ctx)
+		if err == nil {
+			break
+		}
+		i.logger.Error("failed to get block number, retrying...", "error", err)
+		time.Sleep(2 * time.Second)
+	}
+	if err != nil {
+		return 0, err
+	}
+	return blkNum, nil
+}
+
+func (i *infiniteRetryL1Client) HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error) {
+	var hdr *types.Header
+	var err error
+	for retries := 50; retries > 0; retries-- {
+		hdr, err = i.EthClient.HeaderByNumber(ctx, number)
+		if err == nil {
+			break
+		}
+		i.logger.Error("failed to get header by number, retrying...", "error", err)
+		time.Sleep(2 * time.Second)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return hdr, nil
+}
+
+func (i *infiniteRetryL1Client) BlockByNumber(ctx context.Context, number *big.Int) (*types.Block, error) {
+	var blk *types.Block
+	var err error
+	for retries := 50; retries > 0; retries-- {
+		blk, err = i.EthClient.BlockByNumber(ctx, number)
+		if err == nil {
+			break
+		}
+		i.logger.Error("failed to get block by number, retrying...", "error", err)
+		time.Sleep(2 * time.Second)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return blk, nil
+}
 func setBuilderMapping(
 	ctx context.Context,
 	bt *blocktracker.BlocktrackerTransactorSession,
