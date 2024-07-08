15cb5cf40a09f2b866eb96b23c5d669d993b1064
Shawn <44221603+shaspitz@users.noreply.github.com>
2024-07-08 15:12:57 -0700

    fix: keep amount as path param

--- p2p/gen/go/providerapi/v1/providerapi.pb.gw.go
+++ p2p/gen/go/providerapi/v1/providerapi.pb.gw.go
@@ -93,12 +93,29 @@ func request_Provider_SendProcessedBids_0(ctx context.Context, marshaler runtime
 }
 
 var (
-	filter_Provider_RegisterStake_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+	filter_Provider_RegisterStake_0 = &utilities.DoubleArray{Encoding: map[string]int{"amount": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
 )
 
 func request_Provider_RegisterStake_0(ctx context.Context, marshaler runtime.Marshaler, client ProviderClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq StakeRequest
 	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["amount"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "amount")
+	}
+
+	protoReq.Amount, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "amount", err)
+	}
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
@@ -115,6 +132,23 @@ func request_Provider_RegisterStake_0(ctx context.Context, marshaler runtime.Mar
 func local_request_Provider_RegisterStake_0(ctx context.Context, marshaler runtime.Marshaler, server ProviderServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq StakeRequest
 	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["amount"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "amount")
+	}
+
+	protoReq.Amount, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "amount", err)
+	}
 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
@@ -192,7 +226,7 @@ func RegisterProviderHandlerServer(ctx context.Context, mux *runtime.ServeMux, s
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
 		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/providerapi.v1.Provider/RegisterStake", runtime.WithHTTPPathPattern("/v1/provider/register_stake"))
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/providerapi.v1.Provider/RegisterStake", runtime.WithHTTPPathPattern("/v1/provider/register_stake/{amount}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -350,7 +384,7 @@ func RegisterProviderHandlerClient(ctx context.Context, mux *runtime.ServeMux, c
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
 		var err error
 		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/providerapi.v1.Provider/RegisterStake", runtime.WithHTTPPathPattern("/v1/provider/register_stake"))
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/providerapi.v1.Provider/RegisterStake", runtime.WithHTTPPathPattern("/v1/provider/register_stake/{amount}"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
@@ -418,7 +452,7 @@ var (
 
 	pattern_Provider_SendProcessedBids_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "provider", "send_processed_bids"}, ""))
 
-	pattern_Provider_RegisterStake_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "provider", "register_stake"}, ""))
+	pattern_Provider_RegisterStake_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"v1", "provider", "register_stake", "amount"}, ""))
 
 	pattern_Provider_GetStake_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "provider", "get_stake"}, ""))
 
