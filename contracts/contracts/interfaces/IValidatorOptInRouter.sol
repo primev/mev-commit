1107a536614c2e60662aa88ec32d4ace328e9b96
Shawn <44221603+shaspitz@users.noreply.github.com>
2024-07-11 23:24:13 -0700

    chore: standardize solidity version to 0.8.20 across repo (#230)

--- contracts/contracts/interfaces/IValidatorOptInRouter.sol
+++ contracts/contracts/interfaces/IValidatorOptInRouter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSL 1.1
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 interface IValidatorOptInRouter {
     /// @notice Initializes the contract with the validator registry and mev-commit AVS contracts.
