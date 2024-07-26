c75b3dc9e1c4f3a3aaeb5e64bd919f2d8f3faef2
Alok <aloknerurkar@no-reply.com>
2024-07-26 17:04:44 +0530

    fix: upper bound for pebble query

--- p2p/pkg/storage/pebble/pebble.go
+++ p2p/pkg/storage/pebble/pebble.go
@@ -1,6 +1,8 @@
 package pebblestorage
 
 import (
+	"errors"
+
 	"github.com/cockroachdb/pebble"
 	"github.com/primev/mev-commit/p2p/pkg/storage"
 )
@@ -20,7 +22,7 @@ func New(path string) (*pebbleStorage, error) {
 }
 
 func (s *pebbleStorage) Close() error {
-	return s.db.Close()
+	return errors.Join(s.db.Flush(), s.db.Close())
 }
 
 func (s *pebbleStorage) Get(key string) ([]byte, error) {
@@ -43,7 +45,7 @@ func (s *pebbleStorage) Get(key string) ([]byte, error) {
 func (s *pebbleStorage) WalkPrefix(prefix string, fn func(key string, val []byte) bool) error {
 	iter, err := s.db.NewIter(&pebble.IterOptions{
 		LowerBound: []byte(prefix),
-		UpperBound: []byte(prefix + "\xff"),
+		UpperBound: upperBound([]byte(prefix)),
 	})
 	if err != nil {
 		return err
@@ -59,23 +61,23 @@ func (s *pebbleStorage) WalkPrefix(prefix string, fn func(key string, val []byte
 }
 
 func (s *pebbleStorage) Put(key string, value []byte) error {
-	return s.db.Set([]byte(key), value, pebble.Sync)
+	return s.db.Set([]byte(key), value, pebble.NoSync)
 }
 
 func (s *pebbleStorage) Delete(key string) error {
-	return s.db.Delete([]byte(key), pebble.Sync)
+	return s.db.Delete([]byte(key), pebble.NoSync)
 }
 
 func (s *pebbleStorage) DeletePrefix(prefix string) error {
 	batch := s.db.NewBatch()
 	defer batch.Close()
 
-	err := batch.DeleteRange([]byte(prefix), []byte(prefix+"\xff"), nil)
+	err := batch.DeleteRange([]byte(prefix), upperBound([]byte(prefix)), nil)
 	if err != nil {
 		return err
 	}
 
-	return batch.Commit(pebble.Sync)
+	return batch.Commit(pebble.NoSync)
 }
 
 func (s *pebbleStorage) Batch() storage.Batch {
@@ -97,13 +99,34 @@ func (b *pebbleBatch) Delete(key string) error {
 }
 
 func (b *pebbleBatch) DeletePrefix(prefix string) error {
-	return b.batch.DeleteRange([]byte(prefix), []byte(prefix+"\xff"), pebble.Sync)
+	return b.batch.DeleteRange([]byte(prefix), upperBound([]byte(prefix)), pebble.NoSync)
 }
 
 func (b *pebbleBatch) Write() error {
-	return b.batch.Commit(pebble.Sync)
+	return b.batch.Commit(pebble.NoSync)
 }
 
 func (b *pebbleBatch) Reset() {
 	b.batch.Reset()
+}
+func upperBound(prefix []byte) []byte {
+	// if the prefix is 0x01..., we want 0x02 as an upper bound.
+	// if the prefix is 0x0000ff..., we want 0x0001 as an upper bound.
+	// if the prefix is 0x0000ff01..., we want 0x0000ff02 as an upper bound.
+	// if the prefix is 0xffffff..., we don't want an upper bound.
+	// if the prefix is 0xff..., we don't want an upper bound.
+	// if the prefix is empty, we don't want an upper bound.
+	// basically, we want to find the last byte that can be lexicographically incremented.
+	var upper []byte
+	for i := len(prefix) - 1; i >= 0; i-- {
+		b := prefix[i]
+		if b == 0xff {
+			continue
+		}
+		upper = make([]byte, i+1)
+		copy(upper, prefix)
+		upper[i] = b + 1
+		break
+	}
+	return upper
 }
