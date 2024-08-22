653f492cc430960eee39fc4e8cc5a9a02e892075
Shawn <44221603+shaspitz@users.noreply.github.com>
2024-08-21 23:29:18 -0700

    Update testnet.go

--- contracts-abi/config/testnet.go
+++ contracts-abi/config/testnet.go
@@ -10,7 +10,7 @@ type Contracts struct {
 
 type L1Contracts struct {
 	ValidatorOptInRouter string
-	ValidatorRegistryV1  string
+	VanillaRegistry      string
 	MevCommitAVS         string
 }
 
@@ -27,6 +27,6 @@ var TestnetContracts = Contracts{
 
 var HoleskyContracts = L1Contracts{
 	ValidatorOptInRouter: "0xCae46e1013D33587180Db5933Abd75D977c2d7ab",
-	ValidatorRegistryV1:  "0x87D5F694fAD0b6C8aaBCa96277DE09451E277Bcf",
+	VanillaRegistry:      "0x87D5F694fAD0b6C8aaBCa96277DE09451E277Bcf",
 	MevCommitAVS:         "0xEDEDB8ed37A43Fd399108A44646B85b780D85DD4",
 }
