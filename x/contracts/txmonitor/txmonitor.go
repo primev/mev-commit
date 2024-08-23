470988e2708f095aa3e50caa02330405841c6066
Kartik Chopra <k5chopra@edu.uwaterloo.ca>
2024-08-23 16:24:11 -0400

    feat: add info log on txn status

--- x/contracts/txmonitor/txmonitor.go
+++ x/contracts/txmonitor/txmonitor.go
@@ -244,7 +244,7 @@ func (m *Monitor) Sent(ctx context.Context, tx *types.Transaction) {
 		if err := m.saver.Update(context.Background(), tx.Hash(), status); err != nil {
 			m.logger.Error("failed to update transaction", "err", err)
 		}
-		m.logger.Debug("transaction status",
+		m.logger.Info("transaction status",
 			"txHash", tx.Hash(),
 			"txStatus", status,
 		)
