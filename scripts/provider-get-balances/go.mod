ec2992113cb56bc7c612f9817b695fd088f6440c
Mikhail Wall <mikhwall@gmail.com>
2024-11-23 00:43:54 +0400

    fix: split scripts

--- scripts/provider-get-balances/go.mod
+++ scripts/provider-get-balances/go.mod
@@ -1,34 +1,43 @@
-module github.com/primev/mev-commit/scripts
+module github.com/primev/mev-commit/scripts/provider-get-balances
 
 go 1.22.3
 
-require (
-	github.com/ethereum/go-ethereum v1.14.8
-	github.com/primev/mev-commit/contracts-abi v0.0.0-20240812215342-b576754bd6f6
-)
+require github.com/ethereum/go-ethereum v1.14.8
 
 require (
+	github.com/DataDog/zstd v1.5.5 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/bits-and-blooms/bitset v1.10.0 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.4 // indirect
+	github.com/cockroachdb/fifo v0.0.0-20240616162244-4768e80dfb9a // indirect
 	github.com/consensys/bavard v0.1.13 // indirect
 	github.com/consensys/gnark-crypto v0.12.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
 	github.com/crate-crypto/go-kzg-4844 v1.0.0 // indirect
 	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
 	github.com/ethereum/c-kzg-4844 v1.0.0 // indirect
-	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/getsentry/sentry-go v0.28.1 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/holiman/uint256 v1.3.1 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
-	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/prometheus/client_golang v1.19.1 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.55.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/supranational/blst v0.3.11 // indirect
 	github.com/tklauser/go-sysconf v0.3.13 // indirect
 	github.com/tklauser/numcpus v0.7.0 // indirect
+	github.com/urfave/cli/v2 v2.27.1 // indirect
+	github.com/xrash/smetrics v0.0.0-20231213231151-1d8dd44e695e // indirect
 	golang.org/x/crypto v0.25.0 // indirect
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
 	golang.org/x/sync v0.7.0 // indirect
---
+++ scripts/providers-register/main.go
@@ -0,0 +1,201 @@
+package main
+
+import (
+	"context"
+	"crypto/ecdsa"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/big"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+const (
+	rpcURL             = "https://chainrpc.testnet.mev-commit.xyz/"
+	newContractAddress = "0xYourNewContractAddress"
+	providersJSONFile  = "provider_registered_events.json"
+)
+
+type ProviderRegisteredEvent struct {
+	Provider       common.Address `json:"provider"`
+	StakedAmount   *big.Int       `json:"stakedAmount"`
+	BLSPublicKey   string         `json:"blsPublicKey"`
+	CurrentBalance *big.Int       `json:"currentBalance"`
+}
+
+func main() {
+	privateKeyHex := os.Getenv("OWNER_PRIVATE_KEY")
+	if privateKeyHex == "" {
+		log.Fatal("OWNER_PRIVATE_KEY environment variable not set")
+	}
+	privateKey, err := crypto.HexToECDSA(strings.TrimPrefix(privateKeyHex, "0x"))
+	if err != nil {
+		log.Fatalf("Failed to load private key: %v", err)
+	}
+
+	client, err := ethclient.Dial(rpcURL)
+	if err != nil {
+		log.Fatalf("Failed to connect to Ethereum client: %v", err)
+	}
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("Failed to get public key")
+	}
+	ownerAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+	fmt.Printf("Owner Address: %s\n", ownerAddress.Hex())
+
+	events, err := readProvidersFromJSON(providersJSONFile)
+	if err != nil {
+		log.Fatalf("Failed to read providers from JSON: %v", err)
+	}
+
+	err = registerProviders(client, privateKey, ownerAddress, events)
+	if err != nil {
+		log.Fatalf("Failed to register providers: %v", err)
+	}
+
+	fmt.Println("All providers have been registered in the new contract.")
+}
+
+func readProvidersFromJSON(filename string) ([]ProviderRegisteredEvent, error) {
+	fileData, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %v", err)
+	}
+
+	var events []ProviderRegisteredEvent
+	err = json.Unmarshal(fileData, &events)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON data: %v", err)
+	}
+
+	return events, nil
+}
+
+func registerProviders(client *ethclient.Client, privateKey *ecdsa.PrivateKey, ownerAddress common.Address, events []ProviderRegisteredEvent) error {
+	newContractAddr := common.HexToAddress(newContractAddress)
+
+	contractABI, err := abi.JSON(strings.NewReader(`[
+		{
+			"inputs": [
+				{"internalType": "address", "name": "provider", "type": "address"},
+				{"internalType": "bytes[]", "name": "blsPublicKeys", "type": "bytes[]"}
+			],
+			"name": "delegateRegisterAndStake",
+			"outputs": [],
+			"stateMutability": "payable",
+			"type": "function"
+		}
+	]`))
+	if err != nil {
+		return fmt.Errorf("failed to parse new contract ABI: %v", err)
+	}
+
+	nonce, err := client.PendingNonceAt(context.Background(), ownerAddress)
+	if err != nil {
+		return fmt.Errorf("failed to get nonce: %v", err)
+	}
+
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to suggest gas price: %v", err)
+	}
+
+	chainID, err := client.NetworkID(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to get network ID: %v", err)
+	}
+
+	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
+	if err != nil {
+		return fmt.Errorf("failed to create transactor: %v", err)
+	}
+	
+	for _, event := range events {
+		fmt.Printf("Registering provider: %s\n", event.Provider.Hex())
+
+		blsPublicKeyBytes, err := hex.DecodeString(event.BLSPublicKey)
+		if err != nil {
+			return fmt.Errorf("failed to decode BLS public key: %v", err)
+		}
+
+		providerAddress := event.Provider
+		blsPublicKeys := [][]byte{blsPublicKeyBytes}
+
+		input, err := contractABI.Pack("delegateRegisterAndStake", providerAddress, blsPublicKeys)
+		if err != nil {
+			return fmt.Errorf("failed to pack function arguments: %v", err)
+		}
+
+		msg := ethereum.CallMsg{
+			From:     ownerAddress,
+			To:       &newContractAddr,
+			GasPrice: gasPrice,
+			Value:    event.CurrentBalance,
+			Data:     input,
+		}
+		gasLimit, err := client.EstimateGas(context.Background(), msg)
+		if err != nil {
+			gasLimit = uint64(300000)
+			fmt.Printf("Failed to estimate gas limit, using default: %v\n", err)
+		}
+
+		tx := types.NewTransaction(nonce, newContractAddr, event.CurrentBalance, gasLimit, gasPrice, input)
+
+		signedTx, err := auth.Signer(ownerAddress, tx)
+		if err != nil {
+			return fmt.Errorf("failed to sign transaction: %v", err)
+		}
+
+		err = client.SendTransaction(context.Background(), signedTx)
+		if err != nil {
+			return fmt.Errorf("failed to send transaction: %v", err)
+		}
+
+		fmt.Printf("Transaction sent: %s\n", signedTx.Hash().Hex())
+
+		nonce++
+
+		receipt, err := waitForReceipt(client, signedTx.Hash())
+		if err != nil {
+			return fmt.Errorf("failed to get transaction receipt: %v", err)
+		}
+
+		if receipt.Status != types.ReceiptStatusSuccessful {
+			return fmt.Errorf("transaction failed: %s", signedTx.Hash().Hex())
+		}
+
+		fmt.Printf("Provider %s successfully registered.\n", event.Provider.Hex())
+
+		time.Sleep(1 * time.Second)
+	}
+
+	return nil
+}
+
+func waitForReceipt(client *ethclient.Client, txHash common.Hash) (*types.Receipt, error) {
+	for {
+		receipt, err := client.TransactionReceipt(context.Background(), txHash)
+		if err != nil {
+			if err == ethereum.NotFound {
+				time.Sleep(1 * time.Second)
+				continue
+			}
+			return nil, err
+		}
+		return receipt, nil
+	}
+}
