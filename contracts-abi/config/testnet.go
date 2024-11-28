47f560fdd393bdbda4371b04491c34c74ed11043
aloknerurkar <aloknerurkar@gmail.com>
2024-11-28 13:00:08 +0530

    fix: set L1 chain ID for holesky (#509)

--- contracts-abi/config/testnet.go
+++ contracts-abi/config/testnet.go
@@ -1,17 +1,19 @@
 package config
 
 type Contracts struct {
-	BidderRegistry   string
-	ProviderRegistry string
-	PreconfManager   string
-	Oracle           string
-	BlockTracker     string
+	BidderRegistry    string
+	ProviderRegistry  string
+	PreconfManager    string
+	Oracle            string
+	BlockTracker      string
+	SettlementGateway string
 }
 
 type L1Contracts struct {
 	ValidatorOptInRouter string
 	VanillaRegistry      string
 	MevCommitAVS         string
+	L1Gateway            string
 }
 
 var TestnetContracts = Contracts{
