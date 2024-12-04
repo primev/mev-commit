8bfb03c80cde7b56046027974458e99058f01d1c
Alok <aloknerurkar@no-reply.com>
2024-12-04 11:21:34 +0530

    fix: remove required flags for bridge arguments

--- bridge/standard/cmd/user_cli/main.go
+++ bridge/standard/cmd/user_cli/main.go
@@ -46,32 +46,28 @@ var (
 		Required: true,
 	}
 	optionL1RPCUrl = &cli.StringFlag{
-		Name:     "l1-rpc-url",
-		Usage:    "URL for L1 RPC",
-		EnvVars:  []string{"L1_RPC_URL"},
-		Required: true,
-		Value:    "https://ethereum-holesky-rpc.publicnode.com",
+		Name:    "l1-rpc-url",
+		Usage:   "URL for L1 RPC",
+		EnvVars: []string{"L1_RPC_URL"},
+		Value:   "https://ethereum-holesky-rpc.publicnode.com",
 	}
 	optionSettlementRPCUrl = &cli.StringFlag{
-		Name:     "settlement-rpc-url",
-		Usage:    "URL for settlement RPC",
-		EnvVars:  []string{"SETTLEMENT_RPC_URL"},
-		Required: true,
-		Value:    "https://chainrpc.testnet.mev-commit.xyz",
+		Name:    "settlement-rpc-url",
+		Usage:   "URL for settlement RPC",
+		EnvVars: []string{"SETTLEMENT_RPC_URL"},
+		Value:   "https://chainrpc.testnet.mev-commit.xyz",
 	}
 	optionL1ContractAddr = &cli.StringFlag{
-		Name:     "l1-contract-addr",
-		Usage:    "address of the L1 gateway contract",
-		EnvVars:  []string{"L1_CONTRACT_ADDR"},
-		Required: true,
-		Value:    config.HoleskyContracts.L1Gateway,
+		Name:    "l1-contract-addr",
+		Usage:   "address of the L1 gateway contract",
+		EnvVars: []string{"L1_CONTRACT_ADDR"},
+		Value:   config.HoleskyContracts.L1Gateway,
 	}
 	optionSettlementContractAddr = &cli.StringFlag{
-		Name:     "settlement-contract-addr",
-		Usage:    "address of the settlement gateway contract",
-		EnvVars:  []string{"SETTLEMENT_CONTRACT_ADDR"},
-		Required: true,
-		Value:    config.TestnetContracts.SettlementGateway,
+		Name:    "settlement-contract-addr",
+		Usage:   "address of the settlement gateway contract",
+		EnvVars: []string{"SETTLEMENT_CONTRACT_ADDR"},
+		Value:   config.TestnetContracts.SettlementGateway,
 	}
 	optionSilent = &cli.BoolFlag{
 		Name:    "silent",
