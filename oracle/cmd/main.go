6e340b3c5403daeda9294af2d768a2eaad3f6a8e
Mikhail Wall <mikhwall@gmail.com>
2025-02-14 22:36:15 +0100

    fix: lower default base and priority fee (#600)

--- oracle/cmd/main.go
+++ oracle/cmd/main.go
@@ -234,14 +234,14 @@ var (
 		Name:    "gas-tip-cap",
 		Usage:   "Use predefined gas tip cap for transactions",
 		EnvVars: []string{"MEV_COMMIT_GAS_TIP_CAP"},
-		Value:   "1000000000", // 1 gWEI
+		Value:   "50000000", // 0.05 gWEI
 	})
 
 	optionGasFeeCap = altsrc.NewStringFlag(&cli.StringFlag{
 		Name:    "gas-fee-cap",
 		Usage:   "Use predefined gas fee cap for transactions",
 		EnvVars: []string{"MEV_COMMIT_GAS_FEE_CAP"},
-		Value:   "2000000000", // 2 gWEI
+		Value:   "60000000", // 0.06 gWEI
 	})
 
 	optionRelayUrls = altsrc.NewStringSliceFlag(&cli.StringSliceFlag{
