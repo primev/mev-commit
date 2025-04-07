bc4ebddd70f23d58ba6f9b2e8701e7f45d89cf82
Mikhail Wall <mikhwall@gmail.com>
2025-04-08 00:47:38 +0200

    fix: added oracle upgrade (#651)

---
+++ contracts/scripts/core/UpgradeOracle.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: BSL 1.1
+
+// solhint-disable no-console
+// solhint-disable one-contract-per-file
+
+pragma solidity 0.8.26;
+
+import {Script} from "forge-std/Script.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {console} from "forge-std/console.sol";
+
+contract UpgradeOracleImplementation is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        // Retrieve Oracle proxy address from environment variables
+        address oracleProxyAddress = vm.envAddress("ORACLE_PROXY");
+
+        console.log("Current Oracle proxy address:", oracleProxyAddress);
+
+        // Upgrade the Oracle implementation
+        Upgrades.upgradeProxy(
+            oracleProxyAddress,
+            "OracleV2.sol",
+            "" // No initialization needed for upgrade
+        );
+
+        console.log("Oracle implementation upgraded");
+
+        vm.stopBroadcast();
+    }
+}
