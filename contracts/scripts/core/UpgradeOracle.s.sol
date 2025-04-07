680c6dd94c57b2bdc4a92e23b638a34ec266de1b
Mikhail Wall <mikhwall@gmail.com>
2025-04-07 19:05:35 +0200

    fix: fix lint

--- contracts/scripts/core/UpgradeOracle.s.sol
+++ contracts/scripts/core/UpgradeOracle.s.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: BSL 1.1
+
+// solhint-disable no-console
+// solhint-disable one-contract-per-file
+
 pragma solidity 0.8.26;
 
 import {Script} from "forge-std/Script.sol";
@@ -11,18 +15,18 @@ contract UpgradeOracleImplementation is Script {
 
         // Retrieve Oracle proxy address from environment variables
         address oracleProxyAddress = vm.envAddress("ORACLE_PROXY");
-        
+
         console.log("Current Oracle proxy address:", oracleProxyAddress);
-        
+
         // Upgrade the Oracle implementation
         Upgrades.upgradeProxy(
             oracleProxyAddress,
             "Oracle.sol",
-            ""  // No initialization needed for upgrade
+            "" // No initialization needed for upgrade
         );
-        
+
         console.log("Oracle implementation upgraded");
-        
+
         vm.stopBroadcast();
     }
 }
