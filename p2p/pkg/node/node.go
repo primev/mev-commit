da5ba97ece51b06cffc2525e1270bba237e44578
Mikhail Wall <mikhwall@gmail.com>
2025-04-08 16:32:15 +0200

    fix: fixed url order

--- p2p/pkg/node/node.go
+++ p2p/pkg/node/node.go
@@ -88,8 +88,8 @@ var defaultL1URLs = map[string]L1URLs{
 		BeaconAPIURL: "https://ethereum-beacon-api.publicnode.com",
 	},
 	contracts.TestnetChainID.String(): L1URLs{
-		L1RPCURL:     "https://ethereum-holesky-beacon-api.publicnode.com",
-		BeaconAPIURL: "https://ethereum-holesky-rpc.publicnode.com",
+		L1RPCURL:     "https://ethereum-holesky-rpc.publicnode.com",
+		BeaconAPIURL: "https://ethereum-holesky-beacon-api.publicnode.com",
 	},
 }
 
