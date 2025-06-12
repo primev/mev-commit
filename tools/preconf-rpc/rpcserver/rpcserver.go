a0a28583fe34a0b8ced98d21e45915e2a56273c8
Alok <aloknerurkar@no-reply.com>
2025-06-12 19:35:46 +0530

    fix: lint

--- tools/preconf-rpc/rpcserver/rpcserver.go
+++ tools/preconf-rpc/rpcserver/rpcserver.go
@@ -122,8 +122,8 @@ func (s *JSONRPCServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	resp, proxy, err := handler(r.Context(), req.Params...)
 	switch {
 	case err != nil:
-		jsonErr := &JSONErr{}
-		if ok := errors.As(err, jsonErr); ok {
+		var jsonErr *JSONErr
+		if ok := errors.As(err, &jsonErr); ok {
 			// If the error is a JSONErr, we can use it directly.
 			s.writeError(w, req.ID, jsonErr.Code, jsonErr.Message)
 			return
