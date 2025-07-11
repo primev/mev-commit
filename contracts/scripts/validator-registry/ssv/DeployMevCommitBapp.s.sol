5079f098cb64e40b73011f4244f5b6b090d881e5
Shawn <44221603+shaspitz@users.noreply.github.com>
2025-07-11 12:04:37 -0700

    RegisterBapp script

--- contracts/scripts/validator-registry/ssv/DeployMevCommitBapp.s.sol
+++ contracts/scripts/validator-registry/ssv/DeployMevCommitBapp.s.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.29;
 // solhint-disable no-console
 import {Script} from "forge-std/Script.sol";
 import {MevCommitBapp} from "../../../contracts/validator-registry/ssv/MevCommitBapp.sol";
+import {console} from "forge-std/console.sol";
 
 contract DeployMevCommitBapp is Script {
 
@@ -12,9 +13,24 @@ contract DeployMevCommitBapp is Script {
     function run() external {
         require(block.chainid == 560048, "must deploy on hoodi");
         vm.startBroadcast();
-
         MevCommitBapp mevCommitBapp = new MevCommitBapp(SSV_NETWORK, msg.sender);
+        console.log("MevCommitBapp deployed at", address(mevCommitBapp));
+        vm.stopBroadcast();
+    }
+}
 
+contract RegisterBapp is Script {
+    function run() external {
+        require(block.chainid == 560048, "must deploy on hoodi");
+        vm.startBroadcast();
+        MevCommitBapp mevCommitBapp = MevCommitBapp(payable(0x43895A07EF22560e9e1319871870b788e6458797));
+        address[] memory tokens = new address[](1);
+        tokens[0] = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+        mevCommitBapp.registerBApp(
+            tokens,
+            new uint32[](1),
+            "https://github.com/primev/mev-commit/blob/main/static/avs-metadata.json"
+        );
         vm.stopBroadcast();
     }
 }
