a6a33bafc03189cf0e09dcfa1766f841f643b1d7
Owen Raman <owenraman@Owens-MacBook-Pro.local>
2025-07-25 19:24:57 -0400

    add hoodi support to L1 contracts

--- contracts/scripts/validator-registry/middleware/DeployMiddleware.s.sol
+++ contracts/scripts/validator-registry/middleware/DeployMiddleware.s.sol
@@ -11,7 +11,7 @@ import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 import {MevCommitMiddleware} from "../../../contracts/validator-registry/middleware/MevCommitMiddleware.sol";
 import {IRegistry} from "symbiotic-core/interfaces/common/IRegistry.sol";
 import {INetworkRegistry} from "symbiotic-core/interfaces/INetworkRegistry.sol";
-import {SymbioticHoleskyDevnetConsts, SymbioticMainnetConsts} from "./ReleaseAddrConsts.s.sol";
+import {SymbioticHoodiDevnetConsts, SymbioticHoleskyDevnetConsts, SymbioticMainnetConsts} from "./ReleaseAddrConsts.s.sol";
 import {IBaseDelegator} from "symbiotic-core/interfaces/delegator/IBaseDelegator.sol";
 import {INetworkMiddlewareService} from "symbiotic-core/interfaces/service/INetworkMiddlewareService.sol";
 import {MainnetConstants} from "../../MainnetConstants.sol";
@@ -159,6 +159,79 @@ contract DeployHolesky is BaseDeploy {
         mevCommitMiddleware.registerVaults(vaults, slashAmounts);
         console.log("Vault1 (representing wstETH) registered with MevCommitMiddleware with vault addr:",
             address(SymbioticHoleskyDevnetConsts.VAULT_1), "and collateral slash amount:", slashAmounts[0]);
+        
+        vm.stopBroadcast();
+    }
+}
+
+
+contract DeployHoodi is BaseDeploy {
+
+    IRegistry constant public NETWORK_REGISTRY = IRegistry(SymbioticHoodiDevnetConsts.NETWORK_REGISTRY);
+    IRegistry constant public OPERATOR_REGISTRY = IRegistry(SymbioticHoodiDevnetConsts.OPERATOR_REGISTRY);
+    IRegistry constant public VAULT_FACTORY = IRegistry(SymbioticHoodiDevnetConsts.VAULT_FACTORY);
+    IRegistry constant public DELEGATOR_FACTORY = IRegistry(SymbioticHoodiDevnetConsts.DELEGATOR_FACTORY);
+    IRegistry constant public SLASHER_FACTORY = IRegistry(SymbioticHoodiDevnetConsts.SLASHER_FACTORY);
+    IRegistry constant public BURNER_ROUTER_FACTORY = IRegistry(SymbioticHoodiDevnetConsts.BURNER_ROUTER_FACTORY);
+
+    // On Hoodi, use dev keystore account. On mainnet these will be the primev multisig.
+    address constant public EXPECTED_MSG_SENDER = 0x1623fE21185c92BB43bD83741E226288B516134a;
+    address constant public OWNER = EXPECTED_MSG_SENDER;
+    address constant public NETWORK = EXPECTED_MSG_SENDER;
+    address constant public SLASH_ORACLE = EXPECTED_MSG_SENDER; // Temporary placeholder until oracle implements slashing.
+    address constant public SLASH_RECEIVER = EXPECTED_MSG_SENDER; 
+    uint256 constant public MIN_BURNER_ROUTER_DELAY = 2 days;
+
+    uint96 constant public SUBNETWORK_ID = 1;
+    uint256 constant public VAULT1_MAX_NETWORK_LIMIT = 100000 ether;
+    uint256 constant public SLASH_PERIOD_SECONDS = 1 days; // compiles to seconds
+
+    function run() external {
+        require(block.chainid == 23118, "must deploy on Hoodi");
+        require(msg.sender == EXPECTED_MSG_SENDER, "incorrect msg.sender");
+
+        vm.startBroadcast();
+
+        INetworkRegistry networkRegistry = INetworkRegistry(address(NETWORK_REGISTRY));
+        if (!networkRegistry.isEntity(NETWORK)) {
+            networkRegistry.registerNetwork();
+        }
+
+        address mevCommitMiddlewareProxy = deployMevCommitMiddleware(
+            NETWORK_REGISTRY, 
+            OPERATOR_REGISTRY, 
+            VAULT_FACTORY, 
+            DELEGATOR_FACTORY,
+            SLASHER_FACTORY,
+            BURNER_ROUTER_FACTORY,
+            NETWORK, 
+            SLASH_PERIOD_SECONDS, 
+            SLASH_ORACLE, 
+            SLASH_RECEIVER,
+            MIN_BURNER_ROUTER_DELAY,
+            OWNER
+        );
+
+        INetworkMiddlewareService networkMiddlewareService = INetworkMiddlewareService(address(SymbioticHoodiDevnetConsts.NETWORK_MIDDLEWARE_SERVICE));
+        if (networkMiddlewareService.middleware(msg.sender) != address(0)) {
+            console.log("WARNING: overwriting existing middleware registration for network:", msg.sender);
+        }
+        networkMiddlewareService.setMiddleware(mevCommitMiddlewareProxy);
+
+        // No Hoodi Vaults in Symbiotic docs so let's hold off on this
+        // One created here but not verified or used: https://hoodi.etherscan.io/tx/0x3f9e9651b912bf9ac85bfe8e852183f062049b4410a7a94f7dffececb92140bb
+        
+        // IBaseDelegator vault1Delegator = IBaseDelegator(address(SymbioticHoodiDevnetConsts.VAULT_1_DELEGATOR));
+        // vault1Delegator.setMaxNetworkLimit(SUBNETWORK_ID, VAULT1_MAX_NETWORK_LIMIT);
+        // console.log("Vault1 delegator max network limit set to:", VAULT1_MAX_NETWORK_LIMIT);
+        // MevCommitMiddleware mevCommitMiddleware = MevCommitMiddleware(payable(mevCommitMiddlewareProxy));
+        // address[] memory vaults = new address[](1);
+        // vaults[0] = SymbioticHoodiDevnetConsts.VAULT_1;
+        // uint160[] memory slashAmounts = new uint160[](1);
+        // slashAmounts[0] = 0.0001 ether; 
+        // mevCommitMiddleware.registerVaults(vaults, slashAmounts);
+        // console.log("Vault1 (representing wstETH) registered with MevCommitMiddleware with vault addr:",
+        //     address(SymbioticHoodiDevnetConsts.VAULT_1), "and collateral slash amount:", slashAmounts[0]);
         
         vm.stopBroadcast();
     }
