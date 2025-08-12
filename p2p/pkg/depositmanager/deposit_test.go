38f5d4da964559a9ba3d1f0caf7291e671401b06
Shawn <44221603+shaspitz@users.noreply.github.com>
2025-08-11 21:58:59 -0700

    Update deposit_test.go

--- p2p/pkg/depositmanager/deposit_test.go
+++ p2p/pkg/depositmanager/deposit_test.go
@@ -22,15 +22,24 @@ import (
 )
 
 type MockBidderRegistryContract struct {
-	GetDepositFunc func(opts *bind.CallOpts, bidder common.Address, window *big.Int) (*big.Int, error)
+	GetDepositFunc              func(opts *bind.CallOpts, bidder common.Address, provider common.Address) (*big.Int, error)
+	WithdrawalRequestExistsFunc func(opts *bind.CallOpts, bidder common.Address, provider common.Address) (bool, error)
 }
 
 func (m *MockBidderRegistryContract) GetDeposit(
 	opts *bind.CallOpts,
 	bidder common.Address,
-	window *big.Int,
+	provider common.Address,
 ) (*big.Int, error) {
-	return m.GetDepositFunc(opts, bidder, window)
+	return m.GetDepositFunc(opts, bidder, provider)
+}
+
+func (m *MockBidderRegistryContract) WithdrawalRequestExists(
+	opts *bind.CallOpts,
+	bidder common.Address,
+	provider common.Address,
+) (bool, error) {
+	return m.WithdrawalRequestExistsFunc(opts, bidder, provider)
 }
 
 func TestDepositManager(t *testing.T) {
@@ -54,35 +63,45 @@ func TestDepositManager(t *testing.T) {
 		GetDepositFunc: func(
 			opts *bind.CallOpts,
 			bidder common.Address,
-			window *big.Int,
+			provider common.Address,
 		) (*big.Int, error) {
 			return big.NewInt(0), nil
+		},
+		WithdrawalRequestExistsFunc: func(
+			opts *bind.CallOpts,
+			bidder common.Address,
+			provider common.Address,
+		) (bool, error) {
+			return false, nil
 		},
 	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 
-	dm := depositmanager.NewDepositManager(10, st, evtMgr, bidderRegistry, logger)
+	dm := depositmanager.NewDepositManager(st, evtMgr, bidderRegistry, logger)
 	done := dm.Start(ctx)
 
 	// no deposit
-	_, err = dm.CheckAndDeductDeposit(
+	refund, err := dm.CheckAndDeductDeposit(
 		context.Background(),
 		common.HexToAddress("0x123"),
+		common.HexToAddress("0x456"),
 		"10",
-		1,
 	)
 	if err == nil {
 		t.Fatal("expected error")
 	}
-
-	br := &bidderregistry.BidderregistryBidderRegistered{
-		Bidder:          common.HexToAddress("0x123"),
-		DepositedAmount: big.NewInt(100),
-		WindowNumber:    big.NewInt(1),
+	if refund != nil {
+		t.Fatal("expected nil refund")
 	}
 
-	err = publishBidderRegistered(evtMgr, &brABI, br)
+	br := &bidderregistry.BidderregistryBidderDeposited{
+		Bidder:          common.HexToAddress("0x123"),
+		Provider:        common.HexToAddress("0x456"),
+		DepositedAmount: big.NewInt(100),
+	}
+
+	err = publishBidderDeposited(evtMgr, &brABI, br)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -90,56 +109,89 @@ func TestDepositManager(t *testing.T) {
 	for {
 		if val, err := st.GetBalance(
 			common.HexToAddress("0x123"),
-			big.NewInt(1),
-		); err == nil && val != nil && val.Cmp(big.NewInt(10)) == 0 {
+			common.HexToAddress("0x456"),
+		); err == nil && val != nil && val.Cmp(big.NewInt(100)) == 0 {
 			break
 		}
 		time.Sleep(1 * time.Second)
 	}
 
-	for i := int64(1); i <= 10; i++ {
-		// deduct deposit
-		refund, err := dm.CheckAndDeductDeposit(
-			context.Background(),
-			common.HexToAddress("0x123"),
-			"10",
-			i,
-		)
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		// not enough deposit
-		_, err = dm.CheckAndDeductDeposit(
-			context.Background(),
-			common.HexToAddress("0x123"),
-			"10",
-			i,
-		)
-		if err == nil {
-			t.Fatal("expected error")
-		}
-
-		err = refund()
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		// deduct deposit after refund
-		_, err = dm.CheckAndDeductDeposit(
-			context.Background(),
-			common.HexToAddress("0x123"),
-			"10",
-			i,
-		)
-		if err != nil {
-			t.Fatal(err)
-		}
+	// deduct deposit
+	refund, err = dm.CheckAndDeductDeposit(
+		context.Background(),
+		common.HexToAddress("0x123"),
+		common.HexToAddress("0x456"),
+		"100",
+	)
+	if err != nil {
+		t.Fatal(err)
 	}
 
-	publishNewWindow(evtMgr, &btABI, big.NewInt(12))
+	// not enough deposit
+	_, err = dm.CheckAndDeductDeposit(
+		context.Background(),
+		common.HexToAddress("0x123"),
+		common.HexToAddress("0x456"),
+		"10",
+	)
+	if err == nil || !strings.Contains(err.Error(), "insufficient balance") {
+		t.Fatal("expected error for insufficient balance")
+	}
+
+	err = refund()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// deduct deposit after refund
+	_, err = dm.CheckAndDeductDeposit(
+		context.Background(),
+		common.HexToAddress("0x123"),
+		common.HexToAddress("0x456"),
+		"10",
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	balance, err := st.GetBalance(
+		common.HexToAddress("0x123"),
+		common.HexToAddress("0x456"),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if balance == nil || balance.Cmp(big.NewInt(90)) != 0 {
+		t.Fatal("expected balance of 90")
+	}
+
+	publishBidderWithdrawalRequested(evtMgr, &brABI, &bidderregistry.BidderregistryWithdrawalRequested{
+		Bidder:          common.HexToAddress("0x123"),
+		Provider:        common.HexToAddress("0x456"),
+		AvailableAmount: big.NewInt(10),
+		EscrowedAmount:  big.NewInt(10),
+		Timestamp:       big.NewInt(1000),
+	})
+
 	for {
-		count, err := st.BalanceEntries(big.NewInt(1))
+		if val, err := st.GetBalance(
+			common.HexToAddress("0x123"),
+			common.HexToAddress("0x456"),
+		); err == nil && val == nil {
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	publishBidderWithdrawal(evtMgr, &brABI, &bidderregistry.BidderregistryBidderWithdrawal{
+		Bidder:              common.HexToAddress("0x123"),
+		Provider:            common.HexToAddress("0x456"),
+		AmountWithdrawn:     big.NewInt(10),
+		AmountStillEscrowed: big.NewInt(10),
+	})
+
+	for {
+		count, err := st.BalanceEntries(common.HexToAddress("0x123"))
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -149,31 +201,100 @@ func TestDepositManager(t *testing.T) {
 		time.Sleep(1 * time.Second)
 	}
 
+	publishBidderDeposited(evtMgr, &brABI, &bidderregistry.BidderregistryBidderDeposited{
+		Bidder:          common.HexToAddress("0x123"),
+		Provider:        common.HexToAddress("0x456"),
+		DepositedAmount: big.NewInt(777),
+	})
+
+	for {
+		if val, err := st.GetBalance(
+			common.HexToAddress("0x123"),
+			common.HexToAddress("0x456"),
+		); err == nil && val != nil && val.Cmp(big.NewInt(777)) == 0 {
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+
 	cancel()
 	<-done
 }
 
-func publishNewWindow(
-	evtMgr events.EventManager,
-	btABI *abi.ABI,
-	windowNumber *big.Int,
-) {
-	testLog := types.Log{
-		Topics: []common.Hash{
-			btABI.Events["NewWindow"].ID,
-			common.BigToHash(windowNumber),
-		},
-		Data: []byte{},
+func TestStartWithBidderAlreadyDeposited(t *testing.T) {
+	t.Parallel()
+
+	brABI, err := abi.JSON(strings.NewReader(bidderregistry.BidderregistryABI))
+	if err != nil {
+		t.Fatal(err)
 	}
-	evtMgr.PublishLogEvent(context.Background(), testLog)
+
+	btABI, err := abi.JSON(strings.NewReader(blocktracker.BlocktrackerABI))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	logger := util.NewTestLogger(io.Discard)
+	evtMgr := events.NewListener(logger, &btABI, &brABI)
+
+	st := depositstore.New(inmemstorage.New())
+	bidderRegistry := &MockBidderRegistryContract{
+		GetDepositFunc: func(
+			opts *bind.CallOpts,
+			bidder common.Address,
+			provider common.Address,
+		) (*big.Int, error) {
+			if opts.BlockNumber.Cmp(big.NewInt(15)) != 0 {
+				t.Fatal("expected block number 15")
+			}
+			return big.NewInt(33), nil // Existing deposit
+		},
+		WithdrawalRequestExistsFunc: func(
+			opts *bind.CallOpts,
+			bidder common.Address,
+			provider common.Address,
+		) (bool, error) {
+			if opts.BlockNumber.Cmp(big.NewInt(15)) != 0 {
+				t.Fatal("expected block number 15")
+			}
+			return false, nil
+		},
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	dm := depositmanager.NewDepositManager(st, evtMgr, bidderRegistry, logger)
+	done := dm.Start(ctx)
+
+	publishBidderDeposited(evtMgr, &brABI, &bidderregistry.BidderregistryBidderDeposited{
+		Bidder:          common.HexToAddress("0x123"),
+		Provider:        common.HexToAddress("0x456"),
+		DepositedAmount: big.NewInt(100),
+		Raw: types.Log{
+			BlockNumber: 16,
+		},
+	})
+
+	for {
+		if val, err := st.GetBalance(
+			common.HexToAddress("0x123"),
+			common.HexToAddress("0x456"),
+		); err == nil && val != nil && val.Cmp(big.NewInt(133)) == 0 {
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	cancel()
+	<-done
 }
 
-func publishBidderRegistered(
+func publishBidderDeposited(
 	evtMgr events.EventManager,
 	brABI *abi.ABI,
-	br *bidderregistry.BidderregistryBidderRegistered,
+	br *bidderregistry.BidderregistryBidderDeposited,
 ) error {
-	event := brABI.Events["BidderRegistered"]
+	event := brABI.Events["BidderDeposited"]
 	buf, err := event.Inputs.NonIndexed().Pack()
 	if err != nil {
 		return err
@@ -183,8 +304,60 @@ func publishBidderRegistered(
 		Topics: []common.Hash{
 			event.ID,
 			common.HexToHash(br.Bidder.Hex()),
+			common.HexToHash(br.Provider.Hex()),
 			common.BigToHash(br.DepositedAmount),
-			common.BigToHash(br.WindowNumber),
+		},
+		Data:        buf,
+		BlockNumber: br.Raw.BlockNumber,
+	}
+	evtMgr.PublishLogEvent(context.Background(), testLog)
+
+	return nil
+}
+
+func publishBidderWithdrawalRequested(
+	evtMgr events.EventManager,
+	brABI *abi.ABI,
+	br *bidderregistry.BidderregistryWithdrawalRequested,
+) error {
+	event := brABI.Events["WithdrawalRequested"]
+	buf, err := event.Inputs.NonIndexed().Pack(br.AvailableAmount, br.EscrowedAmount)
+	if err != nil {
+		return err
+	}
+
+	testLog := types.Log{
+		Topics: []common.Hash{
+			event.ID,
+			common.HexToHash(br.Bidder.Hex()),
+			common.HexToHash(br.Provider.Hex()),
+			common.BigToHash(br.Timestamp),
+		},
+		Data:        buf,
+		BlockNumber: 1,
+	}
+	evtMgr.PublishLogEvent(context.Background(), testLog)
+
+	return nil
+}
+
+func publishBidderWithdrawal(
+	evtMgr events.EventManager,
+	brABI *abi.ABI,
+	br *bidderregistry.BidderregistryBidderWithdrawal,
+) error {
+	event := brABI.Events["BidderWithdrawal"]
+	buf, err := event.Inputs.NonIndexed().Pack(br.AmountWithdrawn)
+	if err != nil {
+		return err
+	}
+
+	testLog := types.Log{
+		Topics: []common.Hash{
+			event.ID,
+			common.HexToHash(br.Bidder.Hex()),
+			common.HexToHash(br.Provider.Hex()),
+			common.BigToHash(br.AmountWithdrawn),
 		},
 		Data: buf,
 	}
