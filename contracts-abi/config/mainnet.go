c797efc74107e3adc95c4dd4ba239c3be628d717
Shawn <44221603+shaspitz@users.noreply.github.com>
2025-10-01 09:59:54 -0700

    Revert "fix: released contracts (#816)"
    This reverts commit 24d4a374d7a1fcd73530aea9e48aba704628699d.

--- contracts-abi/config/mainnet.go
+++ contracts-abi/config/mainnet.go
@@ -2,21 +2,21 @@ package config
 
 import "math/big"
 
-var MainnetChainID = big.NewInt(57173)
+var MainnetChainID = big.NewInt(8855)
 
 var MevCommitChainContracts = Contracts{
-	BidderRegistry:    "0x145a9f4cbae2ec281f417195ea3464dbd04289a2",
-	ProviderRegistry:  "0xeb6d22309062a86fa194520344530874221ef48c",
-	PreconfManager:    "0x2ee9e88f57a7db801e114a4df7a99eb7257871e2",
-	Oracle:            "0x37a037d2423221f403cfa146f5fb962e19582d90",
-	BlockTracker:      "0x5d64b933739558101f9359e2750acc228f0cb64f",
-	SettlementGateway: "0x21f5f1142200a515248a2eef5b0654581c7f2b46",
+	BidderRegistry:    "0xC973D09e51A20C9Ab0214c439e4B34Dbac52AD67",
+	ProviderRegistry:  "0xb772Add4718E5BD6Fe57Fb486A6f7f008E52167E",
+	PreconfManager:    "0x3761bF3932cD22d684A7485002E1424c3aCCD69c",
+	Oracle:            "0xa1aaCA1e4583dB498D47f3D5901f2B2EB49Bd8f6",
+	BlockTracker:      "0x0DA2a367C51f2a34465ACd6AE5d8A48385e9cB03",
+	SettlementGateway: "0x138c60599946280e5a2DCc1f553B8f0cC0554E03",
 }
 
 var EthereumContracts = L1Contracts{
 	ValidatorOptInRouter: "0x821798d7b9d57dF7Ed7616ef9111A616aB19ed64",
 	VanillaRegistry:      "0x47afdcB2B089C16CEe354811EA1Bbe0DB7c335E9",
 	MevCommitAVS:         "0xBc77233855e3274E1903771675Eb71E602D9DC2e",
-	L1Gateway:            "0x5d64b933739558101f9359e2750acc228f0cb64f",
+	L1Gateway:            "0xDBf24cafF1470a6D08bF2FF2c6875bafC60Cf881",
 	MevCommitMiddleware:  "0x21fD239311B050bbeE7F32850d99ADc224761382",
 }
