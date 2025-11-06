c78792d2276e7f2f63734aba694a169268c160f3
Jason Schwarz <jason.c.schwarz@gmail.com>
2025-11-06 09:12:25 -0400

    fix: CI pipeline error

--- p2p/pkg/rpc/provider/service.go
+++ p2p/pkg/rpc/provider/service.go
@@ -649,7 +649,10 @@ func (s *Service) GetDecryptedTransaction(
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to call shutter sequencer: %v", err)
 	}
-	defer resp.Body.Close()
+	
+	defer func() {
+		_ = resp.Body.Close()
+	}()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
